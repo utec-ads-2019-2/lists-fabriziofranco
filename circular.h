c1b6bc283bb86bb5355f99427f7bf5b30bb0dc7f
fabriziofranco <fabrizio.franco@utec.edu.pe>
2019-09-08 19:30:04 -0500

    Tarea terminada, posible refactoring

--- circular.h
+++ circular.h
@@ -9,47 +9,99 @@ class CircularLinkedList : public List<T> {
         CircularLinkedList() : List<T>() {}
 
         T back() {
-            // TODO
+            if(this->head!= nullptr)
+                return this->head->prev->data;
+            throw out_of_range("This list is empty");
         }
 
         void push_front(T value) {
-            // TODO
+            push_back(value);
+            this->head=this->head->prev;
         }
 
-        void push_back(T value) {
-            // TODO
+         void push_back(T value) {
+            auto nodo_nuevo=new Node<T>();
+            nodo_nuevo->data=value;
+            nodo_nuevo->next=this->head;
+            if(this->head!=nullptr){
+                this->head->prev->next=nodo_nuevo;
+                nodo_nuevo->prev=this->head->prev;
+                this->head->prev=nodo_nuevo;}
+            else{
+                this->head=nodo_nuevo;
+                this->head->next=nodo_nuevo;
+                this->head->prev=nodo_nuevo;
+            }
+            this->nodes++;
         }
 
         void pop_front() {
-            // TODO
+            if(this->head!= nullptr){
+                auto temp=this->head;
+                this->nodes--;
+                if(this->nodes==0){
+                    this->head= nullptr;
+                }
+                else{
+                    this->head->prev->next=this->head->next;
+                    this->head->next->prev=this->head->prev;
+                    this->head=this->head->next;
+                }
+                delete temp;
+            }
         }
+
 
         void pop_back() {
-            // TODO
+            if(this->head!= nullptr){
+                auto temp=this->head->prev;
+                this->nodes--;
+                if(this->nodes==0){
+                    this->head= nullptr;
+                }
+                else{
+                    this->head->prev->prev->next=this->head;
+                    this->head->prev=this->head->prev->prev;
+                }
+                delete temp;
+            }
         }
+
 
         T operator[](int index) {
-            // TODO
+            if(this->head!= nullptr && index<this->nodes){
+                auto contador=0;
+                auto temp=this->head;
+                if(index<=(this->size()/2)){
+                    while(contador!=index){
+                        temp=temp->next;
+                        contador++;
+                    }
+                }
+                else{
+                    while((index+contador)<this->size()){
+                        temp=temp->prev;
+                        contador++;
+                    }
+                }
+                return temp->data;
+            }
+            throw out_of_range("This index doesnt exist");
         }
 
-        bool empty() {
-            // TODO
-        }
-
-        int size() {
-            // TODO
-        }
-
-        void clear() {
-            // TODO
-        }
-
-        void sort() {
-            // TODO
-        }
-    
         void reverse() {
-            // TODO
+            if(this->nodes>1){
+                Node<T>* anterior= this->head->prev;
+                Node<T>* actual= this->head;
+                Node<T>* siguiente= actual->next;
+                for(int i=0;i<this->size();i++){
+                    actual->next=anterior;
+                    actual->prev=siguiente;
+                    anterior=actual;
+                    actual=siguiente;
+                    siguiente=siguiente->next;}
+                this->head=this->head->next;
+            }
         }
 
         string name() {
@@ -65,7 +117,8 @@ class CircularLinkedList : public List<T> {
         }
 
         void merge(CircularLinkedList<T> list) {
-            // TODO
+            for(int i=0;i<list.size();i++){
+                this->push_back(list[i]);}
         }
 };
 
--- main.cpp
+++ main.cpp
@@ -8,5 +8,18 @@ int main(int argc, char *argv[]) {
     cout << "\tLists Practice" << endl;
     cout << "===========================================================" << endl << endl;
     Tester::execute();
+    CircularLinkedList<int> C1;
+    C1.push_back(1);
+    C1.push_back(2);
+    C1.push_back(3);
+    C1.push_back(4);
+    C1.push_back(4);
+    C1.push_back(4);
+    C1.push_back(4);
+    C1.push_back(4);
+    C1.push_back(7);
+    C1.print();
+    C1.reverse();
+    C1.print();
     return EXIT_SUCCESS;
 }
